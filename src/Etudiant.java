3d6adbd7eda1f9cd09822ccc7d31fbbb4c996c5d
Fab-uleux <fabien.turgeon@proton.me>
2025-12-09 13:47:33 -0500

    Etudiant structure, contructeur, getter, toString et method de base Done

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,2 +1,44 @@
 public class Etudiant {
+    private String matricule;
+    private String prenom;
+    private String nom;
+    private char cote;
+    private double achat;
+    private double paye;
+
+    public Etudiant(String matricule, String prenom, String nom, char cote, double achat, double paye) {
+        this.matricule = matricule;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.cote = cote;
+        this.achat = achat;
+        this.paye = paye;
+    }
+
+    public String getMatricule(){
+        return matricule;
+    }
+    public String getPrenom(){
+        return prenom;
+    }
+    public String getNom(){
+        return nom;
+    }
+    public char getCote(){
+        return cote;
+    }
+    public Double getAchat(){
+        return achat;
+    }
+    public Double getPaye(){
+        return paye;
+    }
+
+    @Override
+    public String toString() {
+        return "Etudiant "+"matricule= "+matricule+", prenom= "+prenom+", nom= "+nom+", cote= "+cote+", achat= "+achat+", paye= "+paye;
+    }
+
+    public static void appliquerBonus(){}
+    public static void appliquerPenalite(){}
 }
